f09b25d881b056d79ed8920d264dd59630dc5774
zorarkdouble8 <zorarkdouble8@gmail.com>
2024-10-11 14:01:55 -0400

    Added page and implementing database fetching

---
+++ Application/Login.py
@@ -0,0 +1,4 @@
+#verifies the login and returns a bool
+def verify_login(username, password):
+    print("Verifying")
+    return False
--- Application/Routes.py
+++ Application/Routes.py
@@ -1,6 +1,26 @@
 from flask import render_template
 from Application import app
+from flask import request
+from Application.Login import verify_login
 
 @app.route("/")
 def home():
     return render_template("Home/Home.html")
+
+#TODO replace username with the actual user's username?
+@app.route("/username")
+def userHome():
+    return render_template("User/Home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if (request.method == "POST"):
+        if (verify_login(request.form["username"], request.form["password"]) == False):
+            print("Error, wrong password or username")
+        else:
+            print("Correct user, login")
+        #TODO verify user's credentials
+        print(request.form["username"])
+        return render_template("Home/Home.html")
+    else:
+        return render_template("Login/Login.html")
