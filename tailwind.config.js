12e749dc3d068dd451f94d7492da8ab54dadd2e3
zorarkdouble8 <zorarkdouble8@gmail.com>
2024-10-14 13:33:06 -0400

    Working on tailwind integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: ["./Application/Templates/**/*{html,css}"],
+  content: {
+    relative: true,
+    files: ["./Application/Templates/**/*.{html}"]
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
 
